b10ec23faf6f4ea65069a01b51dde58470594d2b
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-08 03:16:39 +0700

    Add test and debug field for db config

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
+  test: true,
   db: {
     hostName: 'localhost',
     port: 3306,
-    user: 'username_should_be_replaced',
-    password: 'password_should_be_secret',
-    databaseName: 'databaseName_should_be_replaced',
     connectionLimit: 10,
-    multipleStatements: true
+    debug: true
   }
 }
+
+// You can test your own connection using any database from your local

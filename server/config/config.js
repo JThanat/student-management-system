49972d133bb72a370235fd88af2e57f6d175f0a2
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-09 01:57:22 +0700

    Reverse Config File

--- server/config/config.js
+++ server/config/config.js
@@ -3,6 +3,9 @@ module.exports = {
   db: {
     hostName: 'localhost',
     port: 3306,
+    user: 'username_should_be_replaced',
+    password: 'password_should_be_secret',
+    databaseName: 'databaseName_should_be_replaced',
     connectionLimit: 10,
     debug: true
   }

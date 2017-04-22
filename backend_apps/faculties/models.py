d2c0b21602fa22c1d3945aa752568044bec668b8
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-04-23 02:32:00 +0700

    Obtain user info

--- backend_apps/faculties/models.py
+++ backend_apps/faculties/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Faculty(models.Model):
-    fid = models.IntegerField(verbose_name='Faculty ID', primary_key=True, auto_created=True)
+    fid = models.IntegerField(verbose_name='Faculty ID', primary_key=True)
     name = models.CharField(max_length=50, verbose_name='Faculty Name')
 
     def __str__(self):
---
+++ backend_apps/users/utils.py
@@ -0,0 +1,8 @@
+from backend_apps.users.serializers import UserSerializer
+
+
+def jwt_response_payload_handler(token, user=None, request=None):
+    return {
+        'token': token,
+        'user': UserSerializer(user, context={'request': request}).data
+    }

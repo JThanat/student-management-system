459a82b07e72e3b56ca4d42daf0df35c99b88d99
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-04-23 00:37:10 +0700

    Finish user register part

--- backend_config/settings.py
+++ backend_config/settings.py
@@ -64,7 +64,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
 
 TEMPLATES = [

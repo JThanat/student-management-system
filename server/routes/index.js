317367e7e33a05036f8ac41fbaf4bfb706f4814e
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-04-27 02:32:35 +0700

    Rewrite routing path

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router()
 
-router.use('/student', require('./student/student.route'))
+router.use('/activity', require('./activity/activity.route'))
 router.use('/competition', require('./competition/competition.route'))
+router.use('/dashboard', require('./dashboard/dashboard.route'))
+router.use('/leaves', require('./leave/leave.route'))
+router.use('/project', require('./project/project.route'))
+router.use('/punishment', require('./punishment/punishment.route'))
+router.use('/student', require('./student/student.route'))
 router.use('/', (req, res) => {
   res.status(404).send('Page not found')
 })

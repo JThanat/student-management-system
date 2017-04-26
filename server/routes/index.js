53fccdba020877641165bbc6bef1d87bc7c7e50d
Kosate Limpongsa <kosatelim@gmail.com>
2017-04-26 22:47:52 +0700

    Add page 404 status

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 router.use('/student', require('./student/student.route'))
 router.use('/', (req, res) => {
-  res.send('Path not defined')
+  res.status(404).send('Page not found')
 })
 
 module.exports = router

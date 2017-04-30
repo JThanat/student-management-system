3e24fa8e4a5e711c008b67b193b76c92c88b5781
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-05-01 02:09:50 +0700

    Update overtime-student

--- server/routes/dashboard/dashboard.route.js
+++ server/routes/dashboard/dashboard.route.js
@@ -23,7 +23,7 @@ router.get('/average-gpax-year', (req, res) => {
 
 router.get('/average-gpax-all', (req, res) => {
   queryHelper.queryAndResponse({
-    sql: `select avg(gpax) as avg_gpax from students`,
+    sql: `select avg(gpax) as avg_gpax from students;`,
     req: req,
     res: res
   })
@@ -41,9 +41,9 @@ router.get('/leave-student', (req, res) => {
 
 router.get('/overtime-student', (req, res) => {
   queryHelper.queryAndResponse({
-    sql: `select count(*) as leave_count from leaves 
-	        left join students on leaves.sid = students.sid
-          where semester=2 and year=2016`,
+    sql: `select substr(student_id, 1, 2) as academic_year,
+	        count(*) as student_count from students 
+          where convert(substr(student_id, 1, 2), unsigned integer)<56 and status!='G';`,
     req: req,
     res: res
   })

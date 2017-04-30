2488ad26e464496c342f79d8dbf75e0951d16d44
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-05-01 02:44:58 +0700

    Finish dashboard api

--- server/routes/dashboard/dashboard.route.js
+++ server/routes/dashboard/dashboard.route.js
@@ -2,20 +2,23 @@ const express = require('express')
 const queryHelper = require('../../utilities/query')
 const router = express.Router()
 
-router.get('/number-of-student', (req, res) => {
+router.get('/number-of-student-by-year', (req, res) => {
   queryHelper.queryAndResponse({
-    sql: `select substr(student_id, 1, 2) as academic_year,
-	        count(*) as student_count from students 
-          group by substr(student_id, 1, 2)`,
+    sql: `select convert(substr(student_id, 1, 2), unsigned integer)-54 as student_year, 
+          count(*) as student_count from students 
+          where convert(substr(student_id, 1, 2), unsigned integer)-54<=4
+	        group by substr(student_id, 1, 2)`,
     req: req,
     res: res
   })
 })
 
-router.get('/average-gpax-year', (req, res) => {
+router.get('/average-gpax-by-year', (req, res) => {
   queryHelper.queryAndResponse({
-    sql: `select substr(student_id, 1, 2) as academic_year, avg(gpax) as avg_gpax
-	        from students group by SUBSTRING(student_id, 1, 2)`,
+    sql: `select convert(substr(student_id, 1, 2), unsigned integer)-54 as academic_year, 
+	        avg(gpax) as avg_gpax from students 
+          where convert(substr(student_id, 1, 2), unsigned integer)-54 <= 4
+          group by SUBSTRING(student_id, 1, 2);`,
     req: req,
     res: res
   })
@@ -44,6 +47,37 @@ router.get('/overtime-student', (req, res) => {
     sql: `select substr(student_id, 1, 2) as academic_year,
 	        count(*) as student_count from students 
           where convert(substr(student_id, 1, 2), unsigned integer)<56 and status!='G';`,
+    req: req,
+    res: res
+  })
+})
+
+router.get('/student-activity-by-name', (req, res) => {
+  queryHelper.queryAndResponse({
+    sql: `select activity_name, count(*) as student_count from students_activities
+	        left join activities on activities.activity_id = students_activities.activity_id
+          group by activity_name`,
+    req: req,
+    res: res
+  })
+})
+
+router.get('/student-activity-by-year', (req, res) => {
+  queryHelper.queryAndResponse({
+    sql: `select substr(student_id, 1, 2) as academic_year, count(*) as student_count 
+          from students_activities
+	        left join students on students.sid = students_activities.sid
+          group by substr(student_id, 1, 2)`,
+    req: req,
+    res: res
+  })
+})
+
+router.get('/number-of-student-history', (req, res) => {
+  queryHelper.queryAndResponse({
+    sql: `select substr(student_id, 1, 2) as academic_year,
+	        count(*) as student_count from students 
+          group by substr(student_id, 1, 2)`,
     req: req,
     res: res
   })

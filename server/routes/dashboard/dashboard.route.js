2d4ccf0e2fe6e7e453015f8350fdb118c1c1764b
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-05-01 02:08:39 +0700

    Add leave, overtime

--- server/routes/dashboard/dashboard.route.js
+++ server/routes/dashboard/dashboard.route.js
@@ -12,14 +12,41 @@ router.get('/number-of-student', (req, res) => {
   })
 })
 
-router.get('/average-gpax', (req, res) => {
+router.get('/average-gpax-year', (req, res) => {
   queryHelper.queryAndResponse({
     sql: `select substr(student_id, 1, 2) as academic_year, avg(gpax) as avg_gpax
-	        from students group by SUBSTRING(student_id, 1, 2);`,
+	        from students group by SUBSTRING(student_id, 1, 2)`,
     req: req,
     res: res
   })
 })
 
+router.get('/average-gpax-all', (req, res) => {
+  queryHelper.queryAndResponse({
+    sql: `select avg(gpax) as avg_gpax from students`,
+    req: req,
+    res: res
+  })
+})
+
+router.get('/leave-student', (req, res) => {
+  queryHelper.queryAndResponse({
+    sql: `select count(*) as leave_count from leaves 
+	        left join students on leaves.sid = students.sid
+          where semester=2 and year=2016`,
+    req: req,
+    res: res
+  })
+})
+
+router.get('/overtime-student', (req, res) => {
+  queryHelper.queryAndResponse({
+    sql: `select count(*) as leave_count from leaves 
+	        left join students on leaves.sid = students.sid
+          where semester=2 and year=2016`,
+    req: req,
+    res: res
+  })
+})
 
 module.exports = router

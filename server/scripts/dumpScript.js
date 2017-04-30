91996a2cfeaae039eebad32dac1368642dae2fe2
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-04-30 14:35:26 +0700

    Insert things

--- server/scripts/dumpScript.js
+++ server/scripts/dumpScript.js
@@ -23,7 +23,11 @@ db.connect(config.test ? db.MODE_TEST : db.MODE_PRODUCTION, (err) => {
     connection = db.getConnection()
     // dumpFacultyTable(db.getConnection())
     // dumpFacultyMemberTable(db.getConnection())
-    dumpPunishmentCriteria(db.getConnection())
+    // dumpPunishmentCriteria(connection)
+    dumpActivities(connection)
+    dumpStudentsActivities(connection)
+    dumpCompetitionTeams(connection)
+    dumpTeamsStudents(connection)
   }
 })
 // ---------------------------- Faculty  -------------------------
@@ -41,20 +45,7 @@ function dumpFacultyTable () {
     })
   })
 }
-// ---------------------------- Faculty Member -------------------------
-function dumpFacultyMemberTable () {
-  Object.keys(faculties).map((key, index) => {
-    const sql = `INSERT INTO ${SCHEMA} (faculty_id,faculty_name) VALUES (${key},"${faculties[key]}")`
-    console.log(sql)
-    createQueryPromise(sql).then((result) => {
-      console.log(result)
-      return result
-    }).catch((err) => {
-      console.error(err)
-      return err
-    })
-  })
-}
+
 // ---------------------------- Punishment Criteria -------------------------
 const punishments = constant.punishment_criteria
 function dumpPunishmentCriteria () {
@@ -69,3 +60,72 @@ function dumpPunishmentCriteria () {
     })
   })
 }
+
+// ---------------------------- Activities -------------------------
+const activities = constant.activities
+function dumpActivities () {
+  activities.map((activity, index) => {
+    const sql = `INSERT INTO activities (${constant.activities_column.join(',')}) VALUES 
+      ("${activity.activity_name}","${activity.activity_description}",${activity.max_score})`
+    createQueryPromise(sql).then((result) => {
+      console.log(result)
+      return result
+    }).catch((err) => {
+      console.error(err)
+      return err
+    })
+  })
+}
+
+// ------------------------Students Activities -----------------------
+function dumpStudentsActivities () {
+  for (var i = 0; i < activities.length; i++) {
+    for (var j = 0; j < 100; j++) {
+      const sql = `INSERT INTO students_activities 
+        (activity_id, sid, received_score) VALUES 
+        (${i},${Math.floor(Math.random() * 1000)},
+        ${Math.floor(Math.random() * activities[i].max_score)})`
+      createQueryPromise(sql).then((result) => {
+        console.log(result)
+        return result
+      }).catch((err) => {
+        console.error(err)
+        return err
+      })
+    }
+  }
+}
+
+// ---------------------------- Competition Teams -------------------------
+const competition_teams = constant.competition_teams
+function dumpCompetitionTeams () {
+  competition_teams.map((team, index) => {
+    const sql = `INSERT INTO competition_teams (${constant.competition_teams_column.join(',')}) VALUES ("${team.team_name}")`
+    createQueryPromise(sql).then((result) => {
+      console.log(result)
+      return result
+    }).catch((err) => {
+      console.error(err)
+      return err
+    })
+  })
+}
+
+// ----------------------------- Teams Students ----------------------------
+function dumpTeamsStudents () {
+  for (var i = 0; i < competition_teams.length; i++) {
+    for (var j = 0; j < 6; j++) {
+      const sql = `INSERT INTO teams_students 
+        (team_id, sid) VALUES 
+        (${i},${Math.floor(Math.random() * 1000)})`
+      createQueryPromise(sql).then((result) => {
+        console.log(result)
+        return result
+      }).catch((err) => {
+        console.error(err)
+        return err
+      })
+    }
+  }
+}
+
